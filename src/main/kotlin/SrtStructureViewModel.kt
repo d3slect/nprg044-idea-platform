3c38ac7b4dda4da4be6fe57e67559a6c5b7e95aa
Jiri Vinarek <vinarek@d3s.mff.cuni.cz>
2018-05-22 03:34:39 +0200

    minor fix

--- src/main/kotlin/SrtStructureViewModel.kt
+++ src/main/kotlin/SrtStructureViewModel.kt
@@ -1,11 +1,11 @@
 import com.intellij.ide.structureView.*
 import com.intellij.psi.PsiFile
 import cz.cuni.mff.d3s.srtplugin.SrtStructureViewElement
+import cz.cuni.mff.d3s.srtplugin.psi.SrtFile
 
 class SrtStructureViewModel(psiFile: PsiFile) : StructureViewModelBase(psiFile, SrtStructureViewElement(psiFile)),
         StructureViewModel.ElementInfoProvider {
-    //override fun isAlwaysLeaf(element: StructureViewTreeElement?) = element?.value is SrtFile
-    override fun isAlwaysLeaf(element: StructureViewTreeElement?) = false
+    override fun isAlwaysLeaf(element: StructureViewTreeElement?) = element?.value !is SrtFile
 
     override fun isAlwaysShowsPlus(element: StructureViewTreeElement?) = false
 
